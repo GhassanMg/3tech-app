7f197f82dec461df07de77159eda73eef4669369
Ghassan Mg <ghassan.mgsyr@gmail.com>
2025-11-05 16:00:44 +0300

    Update MtnController route to include API versioning

--- src/mtn/mtn.controller.ts
+++ src/mtn/mtn.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { MtnService } from './mtn.service';
 import { AuthenticateDistributorDto } from './dto/authenticate-distributor.dto';
 
-@Controller('mtn')
+@Controller('api/v1/mtn')
 export class MtnController {
   constructor(private readonly mtnService: MtnService) {}
 
